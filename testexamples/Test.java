ae1335376e87cd8c6ecea5cb8ca709142580d8a4
Massimo Pavoni <maspavoni@gmail.com>
2024-05-20 22:45:41 +0200

    Tests with instantiation and let fields

--- testexamples/Test.java
+++ testexamples/Test.java
@@ -9,6 +9,8 @@ public class Test {
                 return f -> g -> x -> f.apply(g.apply(x));
             }
 
+            private final Function<Boolean,Boolean> not = b -> !b;
+
             @Override
             public Void _eval() {
                 System.out.println(this.<Boolean,Boolean,Boolean>compose().apply(FunxPrelude.not()).apply(FunxPrelude.not()).apply(false));
@@ -20,5 +22,11 @@ public class Test {
     @FunctionalInterface
     public interface Let<T> {
         T _eval();
+    }
+
+    public static final Function<Boolean,Boolean> doubleNot = b -> Test.<Boolean,Boolean,Boolean>compose().apply(FunxPrelude.not()).apply(FunxPrelude.not()).apply(b);
+
+    public static <A,B,C> Function<Function<B,C>, Function<Function<A,B>, Function<A,C>>> compose(){
+        return f -> g -> x -> f.apply(g.apply(x));
     }
 }
